d8d9614008dd68970c8b1ca7ab8f3cd58af0ebf9
Yeongho Choi <chldudgh8256@naver.com>
2020-11-02 18:01:44 +0900

    hello

--- src/apollo/Client.js
+++ src/apollo/Client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 export const isLoggedIn = makeVar(localStorage.getItem("token") !== null ? true : false);
 
 const client = new ApolloClient({
-    uri: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "https://nomadgram-backend.herokuapp.com/",
+    uri: "https://nomadgram-backend.herokuapp.com/",
     cache: new InMemoryCache(),
     headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
